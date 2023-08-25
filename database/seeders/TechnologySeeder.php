b42829353a76ed13be133a814d98380b42860f5f
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-25 14:47:10 +0200

    [FEAT] creazione seeder technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\support\Str;
+
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +17,16 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        // CREO I TAG
+        $technologies = ['HTML', 'CSS', 'Javascript','JQuery', 'VueJS', 'Laravel', 'Symfony', 'SASS'];
+
+        // LI CICLO
+        foreach($technologies as $item){
+            $technology= new Technology();
+            $technology->name = $item;
+            $technology->slug =Str::slug($item, '-');
+
+            $technology->save();
+        }
     }
 }
