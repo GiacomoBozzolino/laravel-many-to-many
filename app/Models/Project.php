59818baacc5baea1c75af3a6cdbc928e376b6d99
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-25 15:08:54 +0200

    [FEAT] settati model per relazione many-to-many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,16 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-
+use App\Models\Type;
+use App\Models\Technology;
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable =['name', 'link', 'slug','img', 'type_id'];
+    protected $fillable =['name', 'link', 'slug','img', 'type_id',];
 
     public function type(){
         return $this->belongsTo(Type::class);
     }
 
+
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
+    }
 
     public static function generateSlug($name)
     {
